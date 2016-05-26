d98711b4cdb2f39f8975ab915eaeceb4b44e2bb9
andars <afoote97@gmail.com>
2016-05-25 17:03:23 -0700

    update huffman test

--- tests/huffman.c
+++ tests/huffman.c
@@ -6,8 +6,9 @@
 int main(void) {
     int weights[] = {8, 3, 1, 1, 1, 1, 1, 1};
     char* syms = "abcdefgh";
+    int count = sizeof(weights)/sizeof(int);
 
     huffman code;
-    huffman_init(&code, syms, weights);
-    huffman_encode(&code, "abcdefgh");
+    huffman_init(&code, syms, weights, count);
+    huffman_encode(&code, "abcdefgh", 8);
 }
